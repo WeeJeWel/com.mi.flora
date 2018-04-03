8da926e1c8c8f6d934d259ae8aada5dabe01a7a0
Dennie de Groot <mail@denniedegroot.nl>
2018-04-03 20:32:51 +0200

    Add measure battery capability

--- drivers/ropot/device.js
+++ drivers/ropot/device.js
@@ -5,13 +5,16 @@ const FloraDevice = require('../../lib/FloraDevice.js');
 class FloraDeviceRopot extends FloraDevice {
 	
 	async _onData( data ) {
-		let temperature = data.readUInt16LE(0) / 10;
-		let moisture = data.readUInt16BE(6);
-		let fertility = data.readUInt16LE(8);
-		
-		this.log('Got readings', { temperature, moisture, fertility });
+		let temperature = data[0].readUInt16LE(0) / 10;
+		let moisture = data[0].readUInt16BE(6);
+		let fertility = data[0].readUInt16LE(8);
+		let batteryLevel = parseInt(data[1].toString('hex', 0, 1), 16);
+		let firmwareVersion = data[1].toString('ascii', 2, data[1].length);
+
+		this.log('Got readings', { temperature, moisture, fertility, batteryLevel, firmwareVersion });
 			
 		return Promise.all([
+			this.setCapabilityValue('measure_battery', batteryLevel),
 			this.setCapabilityValue('measure_temperature', temperature),
 			this.setCapabilityValue('flora_measure_moisture', moisture),
 			this.setCapabilityValue('flora_measure_fertility', fertility),
